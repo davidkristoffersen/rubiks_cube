d25d85ce0a202787d1274b1740b1e7db8aca8c7b
David Kristoffersen <david.alta2010@gmail.com>
2018-12-10 16:56:36 +0100

    Template to create ai

--- tester.py
+++ tester.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3.7
 
 from cube import *
-cube = Cube(3)
-print(cube)
-cube.input()
-print(cube)
+from sys import argv
+import re
+
+def parse_args():
+    args = {'size': 3}
+    if len(argv) > 1:
+        assert(re.match(r'^\d+$', argv[1]))
+        args['size'] = int(argv[1])
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    size = args['size']
+
+    cube = Cube(size)
+    print(cube)
+    cube.input()
+    print(cube)
