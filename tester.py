0ae98cdbdf3384a49dce1fe249fc7f9d1b0a2ec2
David Kristoffersen <david.alta2010@gmail.com>
2018-12-10 23:16:33 +0100

    Start of cross solution

--- tester.py
+++ tester.py
@@ -25,18 +25,57 @@ def benchmark(cube):
     for i in range(10000):
         cube.input('l')
 
-def solve(cube):
-    turns = []
-    turns.append('l')
+def center_col(cube, face):
+    return cube[face][1][1]
 
-    cube.input(turns)
+def rotate_to_face_id(cube, face):
+    col = center_col(cube, face)
+    rotate_to_color(cube, col)
+
+def rotate_to_color(cube, col):
+    for k, v in {'r': 'y-', 'l': 'y', 'u': 'x', 'd': 'x-', 'b': 'y2', 'f': ''}.items():
+        if center_col(cube, k) == col:
+            cube.input(v)
+            break
+
+def solve(cube):
+    front_col = 'w'
+    def cross():
+        def edge(color):
+            for face_id, face in cube.items():
+                print(face_id)
+                for y, x in [(0, 1), (1, 0), (1, 2), (2, 1)]:
+                    bro = edge_brother(face_id, face, y, x)
+                    this = face[y][x]
+                    print(this, bro)
+                    if (this == color and bro == 'w') or (this == 'w' and bro == color):
+                        print(this, bro)
+            print()
+
+        def edge_brother(face_id, face, y, x):
+            rotate_to_face_id(cube, face_id)
+            direction = {(0, 1): 'x', (1, 0): 'y', (1, 2): 'y-', (2, 1): 'x-'}[(y,x)]
+            cube.input(direction)
+            newy, newx = {(0, 1): (2, 1), (1, 0): (1, 2), (1, 2): (1, 0), (2, 1): (0, 1)}[(y, x)]
+            tile = face[newy][newx]
+            rotate_to_color(cube, front_col)
+            return tile
+
+        rotate_to_color(cube, front_col)
+        edge('r')
+    cross()
 
 if __name__ == '__main__':
     args = parse_args()
     size = args['size']
     cube = Cube(size)
 
-    solve(cube)
     # checker_board(cube)
     # benchmark(cube)
+
+    cube.scramble()
+    print(cube)
+    solve(cube)
     cube.print_arr()
+    print()
+    print(cube)
