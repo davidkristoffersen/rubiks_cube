83ab5aff5b78b33a1181d38a2ebc233356ac5570
David Kristoffersen <david.alta2010@gmail.com>
2018-12-14 00:13:19 +0100

    Removed cube_ai from repo

--- __init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3.7
-
-from .cube import *
-from .cube_ai import Cube_AI
-from .patterns import Patterns
--- tester.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3.7
-import cube
-
-print(Cube(3))
